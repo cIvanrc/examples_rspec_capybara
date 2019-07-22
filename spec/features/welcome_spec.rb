3487d907a7693c71fe3994d2caceeb05efb7b1dd
ivan <c.ivan.felix.r@gmail.com>
2019-07-22 10:59:23 -0500

    Scenario, check page content customer's link

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 feature "Welcomes", type: :feature do
 
   scenario 'Show "Welcome" message' do
-    visit("/")
+    visit(root_path)
     expect(page).to have_content("Welcome")
+  end
+
+  scenario "Check customer's link" do
+    visit(root_path)
+    expect(page).to have_link("Go to Customers")
   end
 end
