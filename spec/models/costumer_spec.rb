c166f8c2edd47f9650ceb2ca2d7f0a6d3c69f6cc
ivan <c.ivan.felix.r@gmail.com>
2019-12-08 11:59:16 -0600

    added the hound?

--- spec/models/costumer_spec.rb
+++ spec/models/costumer_spec.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Costumer, type: :model do
   context 'costumer name' do
     it '#full_name - overwriting attribute' do
-      costumer = create(:costumer, name: "Ivan")
-      expect(costumer.full_name).to eq("Sr. Ivan")
+      costumer = create(:costumer, name: 'Ivan')
+      expect(costumer.full_name).to eq('Sr. Ivan')
     end
 
     it '#full_name' do
       costumer = create(:costumer)
-      expect(costumer.full_name).to start_with("Sr.")
+      expect(costumer.full_name).to start_with('Sr.')
     end
   end
 
@@ -21,13 +23,13 @@ RSpec.describe Costumer, type: :model do
 
     it '#costumer_male_vip' do
       costumer = create(:costumer_male_vip)
-      expect(costumer.gender).to eq("M")
+      expect(costumer.gender).to eq('M')
       expect(costumer.vip).to be_truthy
     end
 
     it '#costumer_female_vip' do
       costumer = create(:costumer_female_vip)
-      expect(costumer.gender).to eq("F")
+      expect(costumer.gender).to eq('F')
       expect(costumer.vip).to be_truthy
     end
   end
@@ -40,13 +42,13 @@ RSpec.describe Costumer, type: :model do
 
     it '#costumer_male_default' do
       costumer = create(:costumer_male_default)
-      expect(costumer.gender).to eq("M")
+      expect(costumer.gender).to eq('M')
       expect(costumer.vip).to be_falsey
     end
 
     it '#costumer_female_default' do
       costumer = create(:costumer_female_default)
-      expect(costumer.gender).to eq("F")
+      expect(costumer.gender).to eq('F')
       expect(costumer.vip).to be_falsey
     end
   end
@@ -54,16 +56,16 @@ RSpec.describe Costumer, type: :model do
   context 'costumer gender' do
     it '#costumer_male' do
       costumer = create(:costumer_male)
-      expect(costumer.gender).to eq("M")
+      expect(costumer.gender).to eq('M')
     end
 
     it '#costumer_female' do
       costumer = create(:costumer_female)
-      expect(costumer.gender).to eq("F")
+      expect(costumer.gender).to eq('F')
     end
   end
 
   context 'on create' do
-    it { expect{ create(:costumer) }.to change{ Costumer.all.size }.by(1) }
+    it { expect { create(:costumer) }.to change { Costumer.all.size }.by(1) }
   end
 end
