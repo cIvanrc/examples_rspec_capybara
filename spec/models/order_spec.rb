c87414e6e5c142f96c8146b3f9abafe0c3564d0b
ivan <c.ivan.felix.r@gmail.com>
2019-12-18 09:00:11 -0600

    test custom label

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,23 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  it 'it have a order' do
-    order = create(:order)
-    expect(order.costumer).to be_kind_of(Costumer)
-  end
+  context 'message' do
+    it 'it have a order' do
+      order = create(:order)
+      expect(order.costumer).to be_kind_of(Costumer)
+    end
 
-  it 'it have a list of 3 elements' do
-    orders = create_list(:order, 3)
-    expect(orders.count).to eq(3)
-  end
+    it 'it have a list of 3 elements' do
+      orders = create_list(:order, 3)
+      expect(orders.count).to eq(3)
+    end
 
-  it 'has many with change quantity of orders' do
-    costumer = create(:costumer_with_orders, qtt_orders: 5)
-    expect(costumer.orders.count).to eq(5)
-  end
+    it 'has many with change quantity of orders' do
+      costumer = create(:costumer_with_orders, qtt_orders: 5)
+      expect(costumer.orders.count).to eq(5)
+    end
 
-  it 'has many' do
-    costumer = create(:costumer_with_orders)
-    expect(costumer.orders.count).to eq(3)
+    it 'has many' do
+      costumer = create(:costumer_with_orders)
+      expect(costumer.orders.count).to eq(3)
+    end
   end
 end
